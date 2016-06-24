790b21bfcc1636a9acfbfa84a0747eb95726b862
Angel M. Rivera Moreira <rivera.moreira@gmail.com>
2016-06-24 10:56:25 -0600

    Added the ability to search albums by name. Display top 15 search results

--- public/jquery/jquery.js
+++ public/jquery/jquery.js
@@ -3,7 +3,7 @@ console.log('Loaded');
 
 });
 
-$('#albumButton').click(function(e){
+$('#topFive').click(function(e){
     e.preventDefault();
 
     console.log('clicked submit');
@@ -12,34 +12,73 @@ $('#albumButton').click(function(e){
       type: 'GET',
       url: 'http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&limit=5&api_key=41d6d1221c2d58f4e99059abab7a48db&format=json',
       dataType: 'json',
-      success: successCallback
+      success: topFiveSuccessCallback
     }).fail(function(){
-      console.log('FAILED!');
+      console.log('Top Five FAILED!');
     });
 });
 
-var successCallback = function(data){
-  $('#ajaxContent').remove();
+$('#albumButton').click(function(e){
+  e.preventDefault();
+  var album = $('form').serializeArray();
+  console.log('clicked album search: ' + album[0].value);
+  var url = 'http://ws.audioscrobbler.com/2.0/?method=album.search&limit=15&album=' + (album[0].value) + '&api_key=41d6d1221c2d58f4e99059abab7a48db&format=json';
+
+  $.ajax({
+    type: 'GET',
+    url: url,
+    dataType: 'json',
+    success: albumSearchSuccessCallback
+  }).fail(function(){
+    console.log('Album search FAILED!');
+  });
+
+});
+
+var albumSearchSuccessCallback = function(data){
+  $('#albumSearchDiv').empty();
+  console.log(data)
+  var $li = $('<ul>');
+  var $div = $('<div id="albumSearchResultDiv">');
+    $.each(data.results.albummatches.album, function(index, value){
+
+      var $name = $('<li> '+ value.name + ' <a href=" ' + value.url + '" target="_blank"> '  +
+                    value.artist + ' </a><img src="' + value.image[1]["#text"]+ '"> </li><br/>' );
+      console.log(index + ' ' + $name.toString())
+      $li.append($name);
+
+
+    });
+    $('body').append($div);
+    $('#albumSearchResultDiv').append($li);
+
+
+}
+
+var topFiveSuccessCallback = function(data){
+  $('#topFive').remove();
   console.log(data);
   var $table = $('<table>');
-  var $th = $('<th>Name of the Song</th><th>Artist</th><th>All time count</th><th>URL</th>')
+  var $th = $('<th>#</th><th>Name of the Song</th><th>Artist</th><th>All time count</th>')
 
   $table.append($th);
     $.each(data.tracks.track, function(index, value){
 
 
+    var $number = $('<td>' + (index + 1) + '</td>');
+    var $name = $('<td><a href=" ' + value.url + '" target="_blank">' + value.name+ '</a></td>');
+    var $artist = $('<td><a href=" '+ value.artist.url + '" target="_blank"> ' + value.artist.name + '</a></td>')
 
-    var $name = $('<td>' + value.name+ '</td>');
-    var $artist = $('<td>' + value.artist.name + '</td>')
     var $playCount = $('<td>' + value.playcount+ '</td>');
-    var $url = $('<td><a href="' + value.url + '" target="_blank"> '+ value.name + '</td>');
+
     $table.append('<tr>');
+    $table.append($number);
     $table.append($name);
     $table.append($artist);
     $table.append($playCount);
-    $table.append($url);
+
     $table.append('</tr>');
     });
-  console.log('before appending');
+
  $('#topCharts').append($table);
 }
